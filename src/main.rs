836023bdc965e3a4f8596dbaccba28cb8039efd4
RephlexZero <rephlexzero@protonmail.com>
2025-01-07 00:07:28 +0000

    Stuck in Adc::new

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,61 @@
 #![no_std]
 #![no_main]
 
-use defmt::*;
+use defmt::info;
 use embassy_executor::Spawner;
-use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_stm32::adc::{Adc, SampleTime};
+use embassy_stm32::peripherals::ADC1;
+use embassy_stm32::{Config, adc, bind_interrupts};
 use embassy_time::Timer;
 use {defmt_rtt as _, panic_probe as _};
 
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    let p = embassy_stm32::init(Default::default());
-    info!("Hello World!");
+bind_interrupts!(struct Irqs {
+    ADC1_2 => adc::InterruptHandler<ADC1>;
+});
 
-    let mut led = Output::new(p.PA5, Level::High, Speed::Low);
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) -> ! {
+    let mut config = Config::default();
+    {
+        use embassy_stm32::rcc::*;
+
+        config.rcc.hsi = true;
+        config.rcc.pll = Some(Pll {
+            src: PllSource::HSI,
+            prediv: PllPreDiv::DIV1,
+            mul: PllMul::MUL9,
+        });
+        config.rcc.sys = Sysclk::PLL1_P;
+        config.rcc.ahb_pre = AHBPrescaler::DIV1;
+        config.rcc.apb1_pre = APBPrescaler::DIV2;
+        config.rcc.apb2_pre = APBPrescaler::DIV1;
+    }
+    let mut p = embassy_stm32::init(config);
+
+    info!("create adc...");
+
+    let mut adc = Adc::new(p.ADC1, Irqs);
+
+    adc.set_sample_time(SampleTime::CYCLES601_5);
+
+    info!("enable vrefint...");
+
+    let mut vrefint = adc.enable_vref();
+    let mut temperature = adc.enable_temperature();
 
     loop {
-        info!("high");
-        led.set_high();
-        Timer::after_millis(1000).await;
+        let vref = adc.read(&mut vrefint).await;
+        info!("read vref: {} (should be {})", vref, vrefint.value());
 
-        info!("low");
-        led.set_low();
-        Timer::after_millis(1000).await;
+        let temp = adc.read(&mut temperature).await;
+        info!("read temperature: {}", temp);
+
+        let pin = adc.read(&mut p.PA0).await;
+        info!("read pin: {}", pin);
+
+        let pin_mv = (pin as u32 * vrefint.value() as u32 / vref as u32) * 3300 / 4095;
+        info!("computed pin mv: {}", pin_mv);
+
+        Timer::after_millis(500).await;
     }
 }
